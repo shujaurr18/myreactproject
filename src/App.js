8cda0d78196bc8d12c1fca15a788e57ce1a09c94
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 16:13:25 +0500

    update throw props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React,{useState} from 'react';
+
+import {message} from './message.js';
 import './App.css';
 function App() {
 
@@ -8,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+    <message counter={count}/>
     <h1>The value of counter is:{count}</h1>
     <button onClick={()=> setcount(count + 1)
     }>update counter</button>
---
+++ src/message.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export function message(props){
+    return(
+        <h1>value of counter is :{props.counter}</h1>
+    )
+}
