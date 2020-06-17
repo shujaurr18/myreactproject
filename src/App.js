ff6fe389e08f5ec912051ebe775ad109ec892f27
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 15:04:04 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import './App.css';
-import Dineer from './dinner.js';
 function App() {
   return (
     <div className="App">
-    <h3>hy i m shuja ur rehman</h3>
-   <Dineer dishname="wating for dish" sweetdish="rass malai" />
-   <hr>stop </hr>
-   <Dineer dishname="baryani" sweetdish="rass malai" />
-    <br> </br>
-   <Dineer dishname="nehari" sweetdish="ice caream" />
+    <h1>this is aleena shahzadi</h1>
 
 </div>
   );
