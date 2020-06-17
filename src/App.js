cc468c86c758fde99d53d913921089d6fea76c3d
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 06:31:52 +0500

    update at shell

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-<h1>THIS IS MY PROJECT OF REACT</h1>
+<h1>THIS IS MY PROJECT OF REACT!</h1>
 <button  > press enter</button>
 </div>
   );
