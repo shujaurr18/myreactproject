51b057704eb8028d1b37d0ab78bf5c9b60e2056d
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 13:23:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
     <h3>hy i m shuja ur rehman</h3>
    <Dineer dishname="wating for dish" sweetdish="rass malai" />
+   <hr>stop </hr>
    <Dineer dishname="baryani" sweetdish="rass malai" />
+    <br> </br>
    <Dineer dishname="nehari" sweetdish="ice caream" />
 </div>
   );
