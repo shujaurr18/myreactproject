079f1d3204f1cd242d783017496dfc5c1a41654a
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 15:43:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 function App() {
+
+ let [count , setcount] = useState(1);
+
+
+
   return (
     <div className="App">
-    <h1>this is aleena shahzadi</h1>
-
+    <h1>The value of counter is:{count}</h1>
+    <button onClick={()=> setcount(4)
+    }>update counter</button>
 </div>
   );
 }
