7664e191334a28558ca72312cc7ddc9e51181ec0
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 15:56:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React,{useState} from 'react';
 import './App.css';
 function App() {
 
- let [count , setcount] = useState(1);
+ let [count , setcount] = useState(0);
 
 
 
   return (
     <div className="App">
     <h1>The value of counter is:{count}</h1>
-    <button onClick={()=> setcount(++count)
+    <button onClick={()=> setcount(count + 1)
     }>update counter</button>
 </div>
   );
