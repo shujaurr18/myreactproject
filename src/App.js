1bc85325adfc24ecb12b9be96075a105e0340451
shujaurr18 <63506219+shujaurr18@users.noreply.github.com>
2020-06-17 11:59:21 +0500

    add dinner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
+import Dineer from './dinner.js';
+
 function App() {
-  return name ="shuja ur rehman"
-};
+  return (
+    <div>
+<h1>THIS IS MY PROJECT OF REACT!</h1>
+<button  > press enter</button>
+</div>
+  );
+}
+
 export default App;
---
+++ src/dinner.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Dineer(){
+    return(
+<div>
+<h1> Today we are serving chicken baryani </h1>
+<h2> Today we are serving chicken krhai </h2>
+</div>
+
+)
+        
+    
+
+}
+export default Dineer;
